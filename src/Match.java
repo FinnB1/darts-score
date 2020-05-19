0e1227446f467c22b03fcaafb72e4d6c21c81d18
BuildTools <finn.butler@warwick.ac.uk>
2020-05-19 14:55:21 +0100

    stats class added

--- src/Match.java
+++ src/Match.java
@@ -1,10 +1,14 @@
 public class Match {
 
     private int firstTo;
+    private int currentLeg;
+    private int currentSet;
 
     public Match(int firstTo) {
         this.firstTo = firstTo;
     }
+
+
 
 
 }
