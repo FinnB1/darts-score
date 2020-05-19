473cdfbf1e51e4cd555a2b356fcb69a704e277b2
BuildTools <finn.butler@warwick.ac.uk>
2020-05-19 21:44:24 +0100

    leg and set count added

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -12,6 +13,7 @@ public class Main extends Application {
 
         primaryStage.setTitle("dartsScore");
         primaryStage.setScene(new Scene(root, 640, 400));
+        primaryStage.getIcons().add(new Image("assets/dartboard.png"));
         primaryStage.show();
         primaryStage.setResizable(false);
         primaryStage.setMinWidth(primaryStage.getWidth());
